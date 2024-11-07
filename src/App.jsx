232b9c428eaa5e561ef73774f68056864a166b96
zinedine <zine.aitaider@yahoo.com>
2024-11-07 21:36:29 +0100

    react componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Header from './Header'
+import Footer from './Footer'
+import Food from './Food'
+
 import './App.css'
 
 function App() {
+  return(
+    <>
+      <Header>
+      </Header>
+      <Food>
 
+      </Food>
+      <Footer>
+      </Footer>
+    </>
+    
+  );
 }
 
 export default App
---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+ 
+
+function Food(){
+    
+    const food1 = "orange"
+    const food2 = "banana"
+
+    return (
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food
