6f8d5b24fc5ea297c5304a2fadee50736ebf335c
zinedine <zine.aitaider@yahoo.com>
2024-11-08 23:40:12 +0100

    props types and defaults

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import Student from './Student.jsx'
 import './App.css'
 
 function App() {
+    return(
+        <>
+            <Student 
+                name="SpongeBob"
+                age={12}
+                isStudent={true}
+            />
+            
+            <Student 
+                name="Partick"
+                age={30}
+                isStudent={false}
+            />
 
+            <Student 
+                name="Emely"
+                age={25}
+                isStudent={true}
+            />
+
+            <Student 
+                name="Larry"
+            />
+            <Student></Student>
+        </>
+    );
 }
 
 export default App
