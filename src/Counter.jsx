e95c158e0afb0393e9dbbcca2c8bfee12a75b40d
zinedine <zine.aitaider@yahoo.com>
2024-11-13 21:32:34 +0100

    updater function with arrow function

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,15 +4,20 @@ function Counter() {
     const [count, setCount] = useState(0);
     
     const increment = () => {
-        setCount(count+1);
+        setCount( c => c +3);
+        setCount( c => c +1);
+        setCount( c => c +1);
+
     }
 
     const decrement = () => {
-        setCount(count-1);
+        setCount( c => c - 1);
+        setCount( c => c - 1);
+        setCount( c => c - 1);
     }
 
     const reset = () => {
-        setCount(0);
+        setCount(c => c = 0);
     }
 
     const getBackgroundColor =() =>{
